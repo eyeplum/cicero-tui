82b50135bd98d12bc15801fbbbfb35a6d5ab3b7f
Yan Li <eyeplum@gmail.com>
2020-02-12 22:27:36 +1300

    Draw a box when executed :)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdout;
+
+use crossterm::{
+    terminal::{disable_raw_mode, enable_raw_mode},
+    Result,
+};
+
+use tui::{
+    backend::CrosstermBackend,
+    layout::Rect,
+    widgets::{Block, Borders, Widget},
+    Terminal,
+};
+
+fn main() -> Result<()> {
+    enable_raw_mode()?;
+
+    let stdout = stdout();
+
+    let backend = CrosstermBackend::new(stdout);
+
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    terminal.draw(|mut f| {
+        let frame_size = f.size();
+        let size = Rect::new(0, 0, frame_size.width / 2, frame_size.height / 2);
+        Block::default()
+            .title("Block")
+            .borders(Borders::ALL)
+            .render(&mut f, size);
+    })?;
+
+    disable_raw_mode()
 }
