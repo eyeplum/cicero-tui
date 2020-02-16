8f9a381751d1bd38e4d7da96616b77aa5053e513
Yan Li <eyeplum@gmail.com>
2020-02-16 15:26:06 +1300

    Draw 2 boxes :)

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,34 @@
 use std::io::stdout;
 
 use crossterm::{
+    event::{read, Event, KeyCode},
     terminal::{disable_raw_mode, enable_raw_mode},
     Result,
 };
 
 use tui::{
     backend::CrosstermBackend,
-    layout::Rect,
+    layout::{Constraint, Direction, Layout},
     widgets::{Block, Borders, Widget},
     Terminal,
 };
+
+enum InputMode {
+    Normal,
+    Edit,
+}
+
+impl Default for InputMode {
+    fn default() -> Self {
+        InputMode::Normal
+    }
+}
+
+#[derive(Default)]
+struct Application {
+    active_input_mode: InputMode,
+    user_input: String,
+}
 
 fn main() -> Result<()> {
     enable_raw_mode()?;
@@ -20,16 +38,52 @@ fn main() -> Result<()> {
     let backend = CrosstermBackend::new(stdout);
 
     let mut terminal = Terminal::new(backend)?;
+    terminal.hide_cursor()?;
     terminal.clear()?;
 
-    terminal.draw(|mut f| {
-        let frame_size = f.size();
-        let size = Rect::new(0, 0, frame_size.width / 2, frame_size.height / 2);
-        Block::default()
-            .title("Block")
-            .borders(Borders::ALL)
-            .render(&mut f, size);
-    })?;
+    let mut application = Application::default();
 
+    loop {
+        terminal.draw(|mut f| {
+            let chunks = Layout::default()
+                .margin(1)
+                .constraints([Constraint::Length(3), Constraint::Length(0)].as_ref())
+                .direction(Direction::Vertical)
+                .split(f.size());
+
+            Block::default()
+                .borders(Borders::ALL)
+                .render(&mut f, chunks[0]);
+
+            Block::default()
+                .title("Grapheme Clusters")
+                .borders(Borders::ALL)
+                .render(&mut f, chunks[1]);
+        })?;
+
+        match read()? {
+            Event::Key(event) => match event.code {
+                KeyCode::Char('e') => {
+                    application.active_input_mode = InputMode::Edit;
+                }
+                KeyCode::Esc => {
+                    application.active_input_mode = InputMode::Normal;
+                }
+                KeyCode::Char('q') => {
+                    break;
+                }
+                KeyCode::Char(c) => {
+                    application.user_input.push(c);
+                }
+                KeyCode::Backspace => {
+                    application.user_input.pop();
+                }
+                _ => {}
+            },
+            _ => {}
+        }
+    }
+
+    terminal.clear()?;
     disable_raw_mode()
 }
